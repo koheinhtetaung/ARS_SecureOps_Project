87b0bbbaacfb0eed4dd3ca39cac50b980c1806a5
koheinhtetaung <koheinhtetaung94@gmail.com>
2025-01-08 00:28:19 +0800

    modify.tf

--- 3.tfc-create-vault-aws-secret-dynamic-role/data.tf
+++ 3.tfc-create-vault-aws-secret-dynamic-role/data.tf
@@ -10,7 +10,7 @@ data "terraform_remote_state" "vault_admin" {
 }
 
 
-# data "vault_aws_access_credentials" "master_iamadmin_creds" {
-#   backend = vault_aws_secret_backend.aws.path
-#   role    = vault_aws_secret_backend_role.iam_admin_dynamic_role.name
-# }
+data "vault_aws_access_credentials" "master_iamadmin_creds" {
+  backend = vault_aws_secret_backend.aws.path
+  role    = vault_aws_secret_backend_role.iam_admin_dynamic_role.name
+}
